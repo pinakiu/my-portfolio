743210e04e0f99cbfb695bddb05288788cebbf33
Pinakiu <pinaki.u.15@gmail.com>
2025-02-14 22:16:17 -0500

    fix hero section

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -3,11 +3,11 @@ import { RevealOnScroll } from '../RevealOnScroll';
 
 export const Home = () => {
   return (
-    <section id="home" className="min-h-screen flex items-center justify-center relative" style={{ backgroundColor: "#202A25"}}>
+    <section id="home" className="min-h-screen flex items-center justify-center relative">
     <RevealOnScroll>
-        <div className="text-center z-10 px-4">
-            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
-                Hi I'm George.
+        <div className="text-left z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-[#A0ECD0] to--[#406F5E] bg-clip-text text-transparent leading-right">
+                Hi, <br /> I'm George.
             </h1>
 
             <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
