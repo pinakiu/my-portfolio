0aba1b35d2e34cd035e2cb62d1775f1fa89789bc
Pinakiu <pinaki.u.15@gmail.com>
2025-03-06 12:16:56 -0500

    update project colors

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -6,7 +6,7 @@ export const Projects = () => {
     <section id="projects" className="min-h-screen flex items-center justify-center py-20"> 
     <RevealOnScroll>
     <div className="max-w-5xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-purple-400 bg-clip-text text-transparent text-center">Featured Projects</h2>
+        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-[#A0ECD0] to-[#479c7e]  bg-clip-text text-transparent text-center">Featured Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
                 <h3 className="text-xl font-bold mb-2">Cloud Platform</h3>
@@ -17,13 +17,13 @@ export const Projects = () => {
                     {["React", "Node", "AWS", "Docker"].map((tech, key) => (
                         <span 
                         key={key}
-                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition">
+                        className="bg-[#479c7e]/10 text-[#A0ECD0] py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition">
                             {tech}
                         </span>
                     ))}
                 </div>
                 <div className="flex justify-between items-center">
-                    <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4"> View Project -^</a>
+                    <a href="#" className="text-[#479c7e] hover:text-[#A0ECD0] transition-colors my-4"> View Project -^</a>
                 </div>
             </div>
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
@@ -35,13 +35,13 @@ export const Projects = () => {
                     {["Angular", ".NET", "MSSQL", "Harness", "GitHub"].map((tech, key) => (
                         <span
                         key={key}
-                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition">
+                        className="bg-[#479c7e]/10 text-[#A0ECD0] py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition">
                             {tech}
                         </span>
                     ))}
                 </div>
                 <div className="flex justify-between items-center">
-                    <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4"> View Project -^</a>
+                    <a href="#" className="text-[#479c7e] hover:text-[#A0ECD0] transition-colors my-4"> View Project -^</a>
                 </div>
             </div>
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
@@ -53,13 +53,13 @@ export const Projects = () => {
                     {["OpenAI", "Python", "TensorFlow"].map((tech, key) => (
                         <span 
                         key={key}
-                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition">
+                        className="bg-[#479c7e]/10 text-[#A0ECD0] py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition">
                             {tech}
                         </span>
                     ))}
                 </div>
                 <div className="flex justify-between items-center">
-                    <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4"> View Project -^</a>
+                    <a href="#" className="text-[#479c7e] hover:text-[#A0ECD0] transition-colors my-4"> View Project -^</a>
                 </div>
             </div>
         </div>
